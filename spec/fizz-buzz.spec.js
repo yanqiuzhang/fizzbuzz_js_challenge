e2d2c3344d90c9a93441588b531ce21ceab9f603
olivia <olivia@evolve.se>
2019-02-01 22:03:41 +0100

    add a descriptive test title and returns a number if no game rules are met

---
+++ spec/fizz-buzz.spec.js
@@ -0,0 +1,8 @@
+const { FizzBuzz } =  require('../spec.helper')
+
+describe('Fizz Buzz', () => {
+    let fizzBuzz = new FizzBuzz
+    it('returns a number if no game rules are met', () => {
+        expect(fizzBuzz.check(1)).to.eql(1)
+    })
+})
