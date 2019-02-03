5e0b89edc2b863183b053205d564d849efb4e9ce
olivia <olivia@evolve.se>
2019-02-03 14:01:52 +0100

    Add the proper path in the index.html and all test go green but can not open the page after remove src from package.json

--- spec/first.spec.js
+++ spec/first.spec.js
@@ -3,14 +3,11 @@ require('../spec.helper')
 describe('Your test case description', () => {
     let array
     beforeEach(() => {
-        // assign values to your variables
         array = new Array(2, 3)
     });
 
     it('add a descriptive test title', () => {
-        // Execute code if needed
         const sum = array[0] + array[1]
-        // add an assertion using the `expect` keyword
         expect(sum).to.eql(5)
     })
 })
--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -12,5 +12,3 @@ function FizzBuzz()  {
         }
     }
 }
-
-FizzBuzz()
