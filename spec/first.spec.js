2b250257f74b8e77dda611d06de99363b5c3945b
olivia <olivia@evolve.se>
2019-02-03 18:19:33 +0100

    solve the page issue

--- spec/first.spec.js
+++ spec/first.spec.js
@@ -1,18 +1,14 @@
 require('../spec.helper')
 
 describe('Your test case description', () => {
-    // Setup
-    let array
+  let array
 
-    beforeEach(() => {
-        // assign values to your variables
-        array = new Array(2, 3)
-    });
+  beforeEach(() => {
+    array = new Array(2, 3)
+  });
 
-    it('add a descriptive test title', () => {
-        // Execute code if needed
-        const sum = array[0] + array[1]
-        // add an assertion using the `expect` keyword
-        expect(sum).to.eql(5)
-    })
+  it('add a descriptive test title', () => {
+    const sum = array[0] + array[1]
+    expect(sum).to.eql(5)
+  })
 })
